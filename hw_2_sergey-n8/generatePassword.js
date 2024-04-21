ba762a3db569d5515f8aafdc5325341c62799367
Sergey-N8 <keerfeek@gmail.com>
2024-04-21 22:54:06 +0300

    hw_2

---
+++ hw_2_sergey-n8/generatePassword.js
@@ -0,0 +1,14 @@
+function generatePassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    if (length < 6) {
+        return "Длинна пароля минимум 6 симбволов!";
+    } else {
+        let retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+            retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+        return retVal;
+    }
+}
+
+module.exports = { generatePassword };
